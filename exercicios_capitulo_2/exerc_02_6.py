409500977070f56e7c2f612d267a47d465d501b6
paulomarcelonds <paulomarcelonds@gmail.com>
2025-03-17 12:37:11 -0300

    exercicios sobre strings resolvidos

---
+++ exercicios_capitulo_2/exerc_02_6.py
@@ -0,0 +1,13 @@
+"""
+Este é um comentário de múltiplas linhas.
+Python não usa comentários multilinhas nativamente,
+mas strings de múltiplas linhas podem ser usadas assim."
+"""
+
+
+
+name = "Albert Einstein"
+famous_person = "Albert Einstein"
+message = "Uma pessoa que nunca cometeu um erro jamais tentou nada novo"
+new_message = "Uma pessoa que nunca cometeu um erro jamais tentou nada novo"
+print(f'{famous_person}, certa vez disse: {new_message}')
---
+++ exercicios_capitulo_2/exerc_02_7.py
@@ -0,0 +1,7 @@
+
+
+name = " Francisco "
+name_left = name.lstrip()
+name_right = name.rstrip()
+name_both = name.strip()
+print(f"Esse é o nome original: {name}\nEsse nome está sem o espaço inicial: {name_left}.\nEsse nome está sem o espaço final: {name_right}.\nE esse nome está sem os espaços iniciais e sem o espaços finais: {name_both}.")
