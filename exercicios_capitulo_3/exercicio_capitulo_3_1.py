750ba625a564973c0ada5f4bc3d05746da690696
paulomarcelonds <paulomarcelonds@gmail.com>
2025-03-21 13:15:01 -0300

    adiciona arquivos vazios para os exercícios do capítulo 3

---
+++ exercicios_capitulo_3/exercicio_capitulo_3_1.py
@@ -0,0 +1,5 @@
+"""
+3.1 – Nomes: Armazene os nomes de alguns de seus amigos em uma lista
+chamada names. Exiba o nome de cada pessoa acessando cada elemento da
+lista, um de cada vez.
+"""
---
+++ exercicios_capitulo_3/exercicio_capitulo_3_2.py
@@ -0,0 +1,6 @@
+"""
+3.2 – Saudações: Comece com a lista usada no Exercício 3.1, mas em vez de
+simplesmente exibir o nome de cada pessoa, apresente uma mensagem a elas.
+O texto de cada mensagem deve ser o mesmo, porém cada mensagem deve
+estar personalizada com o nome da pessoa.
+"""
